a2e2dca9008c44a319e06a5d4bb5cdd0f277a90a
Dhaval Desai <dhavaldesai1609@gmail.com>
2025-02-03 23:26:09 -0500

    Remove node_modules from tracking

--- cypress/e2e/apiTest.cy.js
+++
@@ -1,9 +0,0 @@
-describe('API Test', () => {
-    it('Fetches user details', () => {
-      cy.request('GET', 'https://jsonplaceholder.typicode.com/users/1').then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.have.property('name');
-      });
-    });
-  });
-  
